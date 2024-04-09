6497b65378cfd8d4ccce12bce2c80d1b1bda447b
Felipe Cesar <felipecesr@gmail.com>
2024-04-09 17:04:22 -0300

    remove button class

--- app/components/button/button.js
+++ app/components/button/button.js
@@ -1,5 +1,5 @@
 import styles from './button.module.css';
 
 export function Button({ children, ...props }) {
-  return <button className={`${styles.button} ${styles['button--block']}`} {...props}>{children}</button>
+  return <button className={styles.button} {...props}>{children}</button>
 }
